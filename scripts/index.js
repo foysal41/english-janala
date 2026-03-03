d0f13338f7d03e2e863bc1d0997b19d21095ae81
foysal41 <mdfoysal.mf33@gmail.com>
2026-03-03 19:39:37 +0600

    handle errors, empty lessons for better ui

--- scripts/index.js
+++ scripts/index.js
@@ -19,13 +19,25 @@ const displayLevelWord=(words)=>{
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
 
+    if(words.length === 0){
+        wordContainer.innerHTML = `
+     <div class="text-center  col-span-full rounded-xl py-10 space-y-6 bangla-font">
+     <img class="mx-auto" src="../assets/alert-error.png" alt="" />
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+    </div>       
+
+        `
+        return;
+    }
+
     words.forEach(word=>{
         const card = document.createElement('div');
         card.innerHTML = `
                     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl ">${word.word}</h2>
-            <p class="font-semibold">${word.pronunciation}</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning}"</div>
+            <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া জায়নি" }</h2>
+            <p class="font-semibold">${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া জায়নি " }</p>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "আরথ পাওয়া জায়নি"}"</div>
 
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
